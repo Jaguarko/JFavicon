2740fafd3470eb281927895aa11e769795c7da95
Jaguar_ko <Savegod1@yandex.ru>
2012-03-14 19:59:55 +0400

    Version 0.2.1

--- jfavicon.js
+++ jfavicon.js
@@ -2,142 +2,121 @@
  * JFavicon - A small library for manipulating the Favicon
  * Str@nnik
  * Copyright (c) 2012 Alex Ivashkin
- * @version 0.1.0
+ * @version 0.2.1
 */
 
 (function() {
 	
-	var CurrentFavicon = null;
-	var OriginalFavicon = null;
-	var OriginalTitle = document.title;
-	var NumNumber = null;
-	var br = function(browser) { return navigator.userAgent.toLowerCase().indexOf(browser) !== -1 };
-	var browser = {
-		chrome: br('chrome'),
-		webkit: br('chrome') || br('safari'),
-		safari: br('safari') && !br('chrome'),
-		mozilla: br('mozilla') && !br('chrome') && !br('safari')
+	var jf = {};
+	var undefined = 'undefined';
+	jf.m = {};
+	jf.init = function (value) {
+		jf.m.body = document.getElementsByTagName('body')[0];
+		jf.m.head = document.getElementsByTagName('head')[0];
+		jf.m.fav = document.querySelectorAll('link[rel="icon"], link[rel="shortcut icon"]')[0];
+		jf.m.title = document.getElementsByTagName('title')[0];
+		
+		if (typeof value != undefined) {
+			if (value.extract) window.jf = jf;
+			if (value.fav) jf.sett.favicon = value.fav;
+			if (value.style) jf.sett.currStyle = value.style;
+			if (value.dir) jf.sett.dir = value.dir;
+		}
+		
+		favicon();
+		
+		return this;
+	}
+	
+	jf.sett = {
+		width: 15,
+		height: 9,
+		bg: '#000',
+		ct: '#fff',
+		num: null,
+		favicon: '/favicon.png',
+		currStyle: 'default',
+		dir: '/'
 	};
 	
-	var isset = {
-		r: function (value) {
-			if (typeof value == 'undefined') {
-				return 0;
-			} else {
-				return 1;
-			}
+	jf.style = {
+		'default': {
+			title: 'default',
+			maxNum: 99
 		},
-		p: function (value, func) {
-			if (typeof value != 'undefined') {
-				func();
-			}
-		},
-		not: function (value, func) {
-			if (typeof value == 'undefined') {
-				func();
+		main: {
+			title: 'main',
+			maxNum: 2,
+			ext: '.png',
+			pref: 'fv'
+		}
+	};
+	
+	jf.setStyle = function (value) {
+		if (typeof value != undefined) {
+			if (typeof jf.style[value.title] == undefined) {
+				jf.style[value.title] = value;
 			}
 		}
 	};
-	var jf = {
-		version: '0.1.0',
-		settings: {
-			width: 16,
-			height: 16,
-			colorBack: '#000000',
-			colorText: '#ffffff',
-			etitle: 1,
-			style: 'main',
-			dir: '/',
-			currentNum: null
-		},
-		set: function (value) {
-			isset.p(value.width, function () { jf.settings.width = value.width });
-			isset.p(value.height, function () { jf.settings.height = value.height });
-			isset.p(value.colorBack, function () { jf.settings.colorBack = value.colorBack });
-			isset.p(value.etitle, function () { jf.settings.etitle = value.etitle });
-			isset.p(value.dir, function () { jf.settings.dir = value.dir });
-			isset.p(value.style, function () { jf.settings.style = value.style });
-			isset.p(value.colorText, function () { jf.settings.colorText = value.colorText });
-			return this;
-		},
-		style: {
-			default: {
-				title: 'default',
-				prIcon: null,
-				maxNum: 99
-			},
-			main: {
-				title: 'main',
-				prIcon: 'fv',
-				maxNum: 2,
-				ext: '.png'
-			}
-		},
-		setStyle: function (value) {
-			console.log(this.style[value.title]);
-			this.style[value.title] = {};
-			this.style[value.title] = {
-				title: value.title,
-				prIcon: value.prIcon,
-				maxNum: value.maxNum,
-				ext: value.ext
-			};
-			return this;
-		}
+	
+	jf.set = function (value) {
+		if (typeof value.width != undefined) jf.sett.width = value.width;
+		if (typeof value.height != undefined) jf.sett.height = value.height;
+		if (typeof value.bg != undefined) jf.sett.bg = value.bg;
+		if (typeof value.ct != undefined) jf.sett.ct = value.ct;
+		if (typeof value.num != undefined) jf.sett.num = value.num;
+		
+		return this;
 	};
+	
+	function favicon () {
+		if (!document.querySelectorAll( 'link[rel="icon"], link[rel="shortcut icon"]')[0]) {
+			var link = document.createElement('link');
+			link.rel = 'shortcut icon';
+			link.href = jf.sett.favicon;
+			link.type = 'image/x-icon';
+			jf.m.head.appendChild(link);
+		} else {
+			jf.m.fvi = document.querySelectorAll( 'link[rel="icon"], link[rel="shortcut icon"]')[0];
+		}
+	}
+	
 	jf.draw = function (num) {
-		if (this.settings.style == 'default') {
-			OriginalFavicon = new Image();
-			OriginalFavicon.src = document.querySelectorAll( 'link[rel="icon"], link[rel="shortcut icon"]' )[0].href;
-			OriginalFavicon.onload = function () {
-				var c = document.createElement('canvas');
-				c.width = 16;
-				c.height = 16;
-				co = c.getContext('2d');
-				console.log(document.querySelectorAll( 'link[rel="icon"], link[rel="shortcut icon"]' )[0].href);
+		if (jf.sett.currStyle == 'default') {
+			var img = new Image();
+			img.src = document.querySelectorAll('link[rel="icon"], link[rel="shortcut icon"]')[0].href;
+			img.onload = function () {
+				var canva = document.createElement('canvas');
+				canva.width = 16;
+				canva.height = 16;
+				var c = canva.getContext("2d");
+				c.drawImage(img, 0, 0);
+				c.fillStyle = jf.sett.bg;
+				c.fillRect(1, 15-jf.sett.height, jf.sett.width, jf.sett.height);
+				c.fillStyle = jf.sett.ct;
+				c.font = "normal 8px sans-serif";
+				c.fillText((num <= 99) ? num : '99+', 2, 13);
+				jf.m.head.removeChild(document.querySelectorAll('link[rel="icon"], link[rel="shortcut icon"]')[0]);
+				var link = document.createElement('link');
+				link.rel = 'shortcut icon';
+				link.href = canva.toDataURL();
+				link.type = 'image/x-icon';
+				jf.m.head.appendChild(link);
 				
-				co.drawImage(OriginalFavicon, 0, 0);
-				co.fillStyle = jf.settings.colorBack;
-				co.fillRect(0, 6, 16, 10);
-				co.fillStyle = jf.settings.colorText;
-				co.font = "normal 9px sans-serif";
-				co.fillText((num <= 99) ? num : '99+', 1, 14);
-				
-				document.querySelector( "head" ).removeChild( document.querySelectorAll( 'link[rel="icon"], link[rel="shortcut icon"]' )[0] );
-				var newicon = document.createElement( "link" );
-				newicon.setAttribute( "rel", "icon" );
-				newicon.setAttribute( "href", c.toDataURL() );
-				newicon.type = 'image/x-icon';
-				document.querySelector( "head" ).appendChild( newicon );
-			};
-			
-		} else {
-			document.querySelector( "head" ).removeChild( document.querySelectorAll( 'link[rel="icon"], link[rel="shortcut icon"]' )[0] );
-			var newicon = document.createElement( "link" );
-			newicon.setAttribute( "rel", "icon" );
-			if (num <= this.style[this.settings.style].maxNum) {
-				newicon.setAttribute( "href", this.settings.dir+this.style[this.settings.style].title+'/'+this.style[this.settings.style].prIcon+num+this.style[this.settings.style].ext)
-			} else {
-				newicon.setAttribute( "href", this.settings.dir+this.style[this.settings.style].title+'/'+this.style[this.settings.style].prIcon+'max'+this.style[this.settings.style].ext)
 			}
-			newicon.type = 'image/x-icon';
-			document.querySelector( "head" ).appendChild( newicon );
-		}
-		jf.settings.currentNum = num;
-	};
-	jf.add = function (num) {
-		if (typeof num != 'undefined') {
-			jf.draw(jf.settings.currentNum+num);
 		} else {
-			jf.draw(jf.settings.currentNum+1);
+			jf.m.head.removeChild(document.querySelectorAll('link[rel="icon"], link[rel="shortcut icon"]')[0]);
+			var link = document.createElement('link');
+			link.rel = 'shortcut icon';
+			var currNum = null;
+			(num <= jf.style[jf.sett.currStyle].maxNum) ? currNum = num : currNum = 'max';
+			link.href = jf.sett.dir + jf.sett.currStyle + '/' + jf.style[jf.sett.currStyle].pref + currNum + jf.style[jf.sett.currStyle].ext;
+			link.type = 'image/x-icon';
+			jf.m.head.appendChild(link);
 		}
-	};
-	jf.deduct = function (num) {
-		if (typeof num != 'undefined') {
-			jf.draw(jf.settings.currentNum-num);
-		} else {
-			jf.draw(jf.settings.currentNum-1);
-		}
+		
+		return this;
 	}
 	
 	window.JFavicon = jf;
